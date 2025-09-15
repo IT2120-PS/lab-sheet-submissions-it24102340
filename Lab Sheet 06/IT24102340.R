b3e7f4a6090acfbab86b7941dd96c8e91dc746ff
it24102340 <it24102340@my.sliit.lk>
2025-09-15 09:48:09 +0530

    LAB 6

---
+++ Lab Sheet 06/IT24102340.R
@@ -0,0 +1,21 @@
+#Exercise
+#1
+#i 
+
+#Binomial Distribution
+#Here, random variable X has binomial distribution with n=50 and p=0.85
+
+#i
+dbinom(46,50,0.85)
+
+#2
+#i.
+# X = number of calls received in one hour
+
+#ii. 
+#Because calls arrive on average 12 per hour and are (assumed) independent, X ~ Poisson (λ=12).
+
+#iii
+dpois(15,12)
+
+
