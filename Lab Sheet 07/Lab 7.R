1cdb2255d718a72aceeac5187eaa27f9599f3370
it24102340 <it24102340@my.sliit.lk>
2025-09-16 11:13:56 +0530

    Lab 7

---
+++ Lab Sheet 07/Lab 7.R
@@ -0,0 +1,24 @@
+
+
+
+#Exercise
+#1
+
+punif(25,min =0,max=40,lower.tail = TRUE)-punif(10,min =0,max=40,lower.tail = TRUE)
+
+#2
+
+pexp(2,rate=0.33,lower.tail=TRUE)
+
+#3
+#i.
+
+1-pnorm(130,mean =100,sd=15,lower.tail=TRUE)
+
+#ii.
+
+qnorm(0.95,mean =100,sd=15,lower.tail=TRUE)
+
+
+
+
